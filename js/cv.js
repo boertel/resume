f8dd45e6fb8cb681274269e728d7f9bef0543cd7
boertel <ben@punchtab.com>
2015-02-18 09:19:02 -0800

    update header

--- js/cv.js
+++ js/cv.js
@@ -1,17 +1,22 @@
 function load_json(language) {
     $.getJSON('json/' + language + '/data.json', function (data) {
 
-        $('#title > h3').html(data.job);
 
         var templateHeader = new Barbe.View('template-social', {data:
             {
                 name: data.name,
-                social: data.social
+                title: data.job
             }
         }).grow();
 
-        var templatePersonal = new Barbe.View("template-personal", {data:
-            {personal:data.personal}
+        var templatePersonal = new Barbe.View(
+            "template-personal",
+            {
+                data:
+                {
+                    personal: data.personal,
+                    social: data.social
+                }
         }).grow();
 
         var templateCategory = new Barbe.View("template-category", {data: data}).grow(function () {
