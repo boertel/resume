492b03b343a151ea8d251aea908fcf56d26f348a
boertel <ben@punchtab.com>
2015-02-26 11:34:18 -0800

    rely on hash to switch between language

--- js/cv.js
+++ js/cv.js
@@ -1,22 +1,10 @@
 function load_json(language) {
+    language = language || document.location.hash.replace('#', '') || 'english';
+
     $.getJSON('json/' + language + '/data.json', function (data) {
+        var templateHeader = new Barbe.View('template-title', {data: data}).grow();
 
-        var templateHeader = new Barbe.View('template-social', {data:
-            {
-                name: data.name,
-                title: data.job
-            }
-        }).grow();
-
-        var templatePersonal = new Barbe.View(
-            "template-personal",
-            {
-                data:
-                {
-                    personal: data.personal,
-                    social: data.social
-                }
-        }).grow();
+        var templatePersonal = new Barbe.View("template-personal", {data: data}).grow();
 
         var templateCategory = new Barbe.View("template-category", {data: data}).grow(function () {
             $('#' + language).find('.title').addClass('fluo');
@@ -25,15 +13,6 @@ function load_json(language) {
     });
 }
 
-$(document).on('click', '.language li', function (e) {
-    e.preventDefault();
-    var language = $(this).attr('id');
-    if (language) {
-        load_json(language);
-    }
-    return false;
-});
+window.addEventListener('hashchange', load_json);
 
-
-// Main
-load_json('english');
+load_json();
