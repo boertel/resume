0468e24cd962268b287a16bf93eee873ffe957cd
boertel <ben@punchtab.com>
2015-03-04 13:15:20 -0800

    data are in data folder

--- js/cv.js
+++ js/cv.js
@@ -1,7 +1,8 @@
 function load_json(language) {
     language = language || document.location.hash.replace('#', '') || 'english';
 
-    $.getJSON('json/' + language + '/data.json', function (data) {
+    var file = language + '.json';
+    $.getJSON('data/' + file, function (data) {
         var templateHeader = new Barbe.View('template-title', {data: data}).grow();
 
         var templatePersonal = new Barbe.View("template-personal", {data: data}).grow();
