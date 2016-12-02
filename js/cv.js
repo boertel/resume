f563cee0a024b5c793c9316eff7c28a4208eef02
boertel <benjamin.oertel@gmail.com>
2016-12-02 12:01:25 -0800

    duration of the job in the title

--- js/cv.js
+++ js/cv.js
@@ -3,12 +3,24 @@ function load_json() {
 
     var file = language + '.json';
     var success = function (data) {
+        data.category[0].experience.forEach(function (category) {
+            var date = {};
+            date.start = moment(category.date.start);
+            date.end = moment(category.date.end);
+            var startFormat = 'MMMM YYYY';
+            if (date.start.year() === date.end.year()) {
+                startFormat = 'MMMM';
+            }
+            category.date.start = date.start.format(startFormat);
+            category.date.end = category.date.end ? date.end.format('MMMM YYYY') : 'present';
+            category.date.duration = date.end.from(date.start, true);
+        });
+
         var templateHeader = new Barbe.View('template-title', {data: data}).grow();
 
         var templatePersonal = new Barbe.View("template-personal", {data: data}).grow();
 
         var templateCategory = new Barbe.View("template-category", {data: data}).grow(function () {
-            $('#' + language).find('.title').addClass('fluo');
         });
 
     };
