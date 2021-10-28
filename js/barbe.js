9dd4363bbc07d004116d2f5905773418c0eedc40
boertel <ben@comediadesign.com>
2021-10-27 18:20:24 -0700

    clean up and pick what you want

--- js/barbe.js
+++ js/barbe.js
@@ -1,295 +1,313 @@
-/* 
+/*
  * @author: Benjamin Oertel (https://github.com/boertel)
  * @date: 01/01/2012
- * 
-*/
+ *
+ */
 (function (window, undefined) {
-    "Barbe:nomunge";
+  "Barbe:nomunge";
 
-    /**
-     * Constructor: 
-     *
-     * @param anchor {Node} Element where the loader is added
-     */
-    var Loader = function (anchor) {
-        this.anchor = anchor;
-        this.div = document.createElement("div");
-        this.div.className = Loader.className;
-        // Clean up the anchor
-        while (this.anchor.hasChildNodes()) {
-            this.anchor.removeChild(this.anchor.lastChild);
-        }
-        this.anchor.appendChild(this.div);
-    };
-    Loader.className = "barbe-loader";
+  /**
+   * Constructor:
+   *
+   * @param anchor {Node} Element where the loader is added
+   */
+  var Loader = function (anchor) {
+    this.anchor = anchor;
+    this.div = document.createElement("div");
+    this.div.className = Loader.className;
+    // Clean up the anchor
+    while (this.anchor.hasChildNodes()) {
+      this.anchor.removeChild(this.anchor.lastChild);
+    }
+    this.anchor.appendChild(this.div);
+  };
+  Loader.className = "barbe-loader";
 
-    /**
-     * Remove the loader from the anchor
-     */
-    Loader.prototype.remove = function () {
-        this.anchor.removeChild(this.div);
-    };
+  /**
+   * Remove the loader from the anchor
+   */
+  Loader.prototype.remove = function () {
+    this.anchor.removeChild(this.div);
+  };
 
-
-    var Barbe = {
-        version: "0.0.2",
-        html: {},
-        templates: {},
-        settings: {
-            template: {
-                render: function (self, context, partials) {
-                    return self.render(context, partials);
-                },
-                compile: function (str) {
-                    return Hogan.compile(str);
-                },
-                type: ['text/html']
-            },
-            removeScripts: true,
-            autoLoad: false,
-            ajax: $.ajax,
-            growCallback: function () {
-            },
-            Loader: Loader
+  var Barbe = {
+    version: "0.0.2",
+    html: {},
+    templates: {},
+    settings: {
+      template: {
+        render: function (self, context, partials) {
+          return self.render(context, partials);
         },
-
-        /**
-         * Initalize template in a dictionary: add the render function to create the final result.
-         *
-         * @param name         {string} name of the template
-         * @param str_template {string} template itself
-         * @param [anchor]     {string} id of the anchor
-         */
-        add: function (name, str_template, anchor) {
-            if (Barbe.html.hasOwnProperty(name)) {
-                throw "[Barbe] You've already got a template by the name: \"" + name + "\"";
-            }
-            else {
-                if (Barbe.settings.template.compile !== undefined) {
-                    Barbe.html[name] = Barbe.settings.template.compile(str_template, name);
-                } else {
-                    Barbe.html[name] = str_template;
-                }
-                Barbe.templates[name] = {};
-
-                if (typeof anchor !== "undefined") {
-                    var element = document.getElementById(anchor);
-                    if (element !== null) {
-                        Barbe.templates[name].anchor = element;
-                    }
-                }
-                Barbe.templates[name].render = function (data, anchor) {
-                    data = data || {};
-                    anchor = anchor || Barbe.templates[name].anchor;
-                    var html = Barbe.settings.template.render(Barbe.html[name], data, Barbe.html);
-                    if (anchor !== undefined) {
-                        anchor.innerHTML = html;
-                    }
-                    return html;
-                };
-            }
+        compile: function (str) {
+          return Hogan.compile(str);
         },
-
-        /**
-         * Parse the html to collect templates defined by
-         * <script type="<Barbe.settings.template.type>" id="" [data-anchor=""]></script>
-         * or grab a specific template
-         *
-         * @params [id] {string} grab a specific template
-         */
-        grab: function (id) {
-            var s, scripts = [];
-
-            if (id === undefined) {
-                scripts = document.scripts || document.getElementsByTagName('script');
-            } else {
-                scripts.push(document.getElementById(id));
-            }
-
-            for(var i = 0, len = scripts.length; i < len; i++) {
-                s = scripts[i];
-
-                if (s !== undefined && s !== null && Barbe.settings.template.type.indexOf(s.type) !== -1) {
-                    Barbe.add(s.id, s.innerHTML, s.getAttribute("data-anchor"));
-                    if (Barbe.settings.removeScripts !== false) {
-                        s.parentNode.removeChild(s);
-                    }
-                }
-            }
-        }
-    };
-
+        type: ["text/html"],
+      },
+      removeScripts: true,
+      autoLoad: false,
+      ajax: fetch,
+      growCallback: function () {},
+      Loader: Loader,
+    },
 
     /**
-     * Constructor
+     * Initalize template in a dictionary: add the render function to create the final result.
      *
-     * @param template                  {string}    template name
-     * @param provider.data             {object}    data that populates the template
-     * @param provider.url              {string}    url of the api
-     * @param provider.*                {*}         ajax function parameters
-     * @param [options.anchor]          {string}    id of the anchor (overwrite the one defined on the template script tag)
-     * @param [options.loader=true]     {boolean}   show/hide the loader
+     * @param name         {string} name of the template
+     * @param str_template {string} template itself
+     * @param [anchor]     {string} id of the anchor
      */
-    Barbe.View = function (template, provider, options) {
-        if (template !== undefined && Barbe.templates[template] === undefined) {
-            Barbe.grab(template);
+    add: function (name, str_template, anchor) {
+      if (Barbe.html.hasOwnProperty(name)) {
+        throw (
+          "[Barbe] You've already got a template by the name: \"" + name + '"'
+        );
+      } else {
+        if (Barbe.settings.template.compile !== undefined) {
+          Barbe.html[name] = Barbe.settings.template.compile(
+            str_template,
+            name
+          );
+        } else {
+          Barbe.html[name] = str_template;
         }
-        if (template === undefined || Barbe.templates[template] === undefined) {
-            throw "[Barbe] template #" + template + " not found.";
+        Barbe.templates[name] = {};
+
+        if (typeof anchor !== "undefined") {
+          var element = document.getElementById(anchor);
+          if (element !== null) {
+            Barbe.templates[name].anchor = element;
+          }
         }
-        this.template = template;
-        this.anchor = Barbe.templates[template].anchor;
-
-        this.options = {};
-
-        // Define default options
-        this.options.Loader = Barbe.settings.Loader;
-
-        var anchor;
-        
-        if (typeof options === "string") {
-            anchor = options;
-        } else if (typeof options !== "undefined") {
-            anchor = options.anchor;
-            delete options.anchor;
-            this.options = options;
-            this.options.Loader = (options.Loader === true || options.Loader === undefined) ?
-                Barbe.settings.Loader : options.Loader;
-        }
-
-        // Is the Loader a valid object?
-        if (this.options.Loader !== false && 
-                (typeof this.options.Loader !== "function" || this.options.Loader.prototype.remove === undefined)) {
-            throw "[Barbe] Invalid Loader";
-        }
-
-        if (anchor !== undefined) {
-            this.anchor = document.getElementById(anchor);
-            if (this.anchor === null) {
-                throw "[Barbe] anchor #" + anchor + " not found.";
-            }
-        }
-
-        this.provider = provider || {};
-    };
+        Barbe.templates[name].render = function (data, anchor) {
+          data = data || {};
+          anchor = anchor || Barbe.templates[name].anchor;
+          var html = Barbe.settings.template.render(
+            Barbe.html[name],
+            data,
+            Barbe.html
+          );
+          if (anchor !== undefined) {
+            anchor.innerHTML = html;
+          }
+          return html;
+        };
+      }
+    },
 
     /**
-     * Populate the template with the data. Create a dumb object {array: data} if data is an array
+     * Parse the html to collect templates defined by
+     * <script type="<Barbe.settings.template.type>" id="" [data-anchor=""]></script>
+     * or grab a specific template
      *
-     * @param response {object} data that populates the template
+     * @params [id] {string} grab a specific template
      */
-    Barbe.View.prototype.render = function (response) {
-        /* Mustache doesn't like array as data, so we have to create 
+    grab: function (id) {
+      var s,
+        scripts = [];
+
+      if (id === undefined) {
+        scripts = document.scripts || document.getElementsByTagName("script");
+      } else {
+        scripts.push(document.getElementById(id));
+      }
+
+      for (var i = 0, len = scripts.length; i < len; i++) {
+        s = scripts[i];
+
+        if (
+          s !== undefined &&
+          s !== null &&
+          Barbe.settings.template.type.indexOf(s.type) !== -1
+        ) {
+          Barbe.add(s.id, s.innerHTML, s.getAttribute("data-anchor"));
+          if (Barbe.settings.removeScripts !== false) {
+            s.parentNode.removeChild(s);
+          }
+        }
+      }
+    },
+  };
+
+  /**
+   * Constructor
+   *
+   * @param template                  {string}    template name
+   * @param provider.data             {object}    data that populates the template
+   * @param provider.url              {string}    url of the api
+   * @param provider.*                {*}         ajax function parameters
+   * @param [options.anchor]          {string}    id of the anchor (overwrite the one defined on the template script tag)
+   * @param [options.loader=true]     {boolean}   show/hide the loader
+   */
+  Barbe.View = function (template, provider, options) {
+    if (template !== undefined && Barbe.templates[template] === undefined) {
+      Barbe.grab(template);
+    }
+    if (template === undefined || Barbe.templates[template] === undefined) {
+      throw "[Barbe] template #" + template + " not found.";
+    }
+    this.template = template;
+    this.anchor = Barbe.templates[template].anchor;
+
+    this.options = {};
+
+    // Define default options
+    this.options.Loader = Barbe.settings.Loader;
+
+    var anchor;
+
+    if (typeof options === "string") {
+      anchor = options;
+    } else if (typeof options !== "undefined") {
+      anchor = options.anchor;
+      delete options.anchor;
+      this.options = options;
+      this.options.Loader =
+        options.Loader === true || options.Loader === undefined
+          ? Barbe.settings.Loader
+          : options.Loader;
+    }
+
+    // Is the Loader a valid object?
+    if (
+      this.options.Loader !== false &&
+      (typeof this.options.Loader !== "function" ||
+        this.options.Loader.prototype.remove === undefined)
+    ) {
+      throw "[Barbe] Invalid Loader";
+    }
+
+    if (anchor !== undefined) {
+      this.anchor = document.getElementById(anchor);
+      if (this.anchor === null) {
+        throw "[Barbe] anchor #" + anchor + " not found.";
+      }
+    }
+
+    this.provider = provider || {};
+  };
+
+  /**
+   * Populate the template with the data. Create a dumb object {array: data} if data is an array
+   *
+   * @param response {object} data that populates the template
+   */
+  Barbe.View.prototype.render = function (response) {
+    /* Mustache doesn't like array as data, so we have to create 
         a dumb object named "array" that contained the array */
-        if (typeof response === "undefined") {
-            response = this.provider.data;
+    if (typeof response === "undefined") {
+      response = this.provider.data;
+    }
+    if (
+      this.provider.url === undefined &&
+      this.provider.success !== undefined
+    ) {
+      response = this.provider.success(response);
+    }
+
+    if (Object.prototype.toString.call(response) === "[object Array]") {
+      response = {
+        array: response,
+      };
+    }
+
+    this.view = Barbe.templates[this.template].render(response, this.anchor);
+    return this.view;
+  };
+
+  /**
+   *  Render the template with the response and attach it to the anchor
+   *
+   *  @params response {object}       api response or data for the template
+   *  @params callback {castAnchor}   function called after
+   */
+  Barbe.View.prototype.castAnchor = function (response, callback) {
+    // TODO move this to the render function
+    this.loader && this.loader.remove();
+    var rendered = this.render(response);
+    callback && callback.call(this, rendered);
+    Barbe.settings.growCallback && Barbe.settings.growCallback(rendered);
+  };
+
+  /**
+   * Process the ajax call
+   *
+   * @param callback {function} function executed when the ajax call successed and the template has been populated
+   */
+  Barbe.View.prototype.ajax = function (callback) {
+    var that = this;
+    if (typeof this.provider === "function") {
+      // Specific asynchronous function for this View
+      var monkeyProvider = function (response) {
+        that.castAnchor.call(that, response, callback);
+      };
+      this.provider.call(this, monkeyProvider);
+    } else {
+      // Use the global ajax function
+      var providerSuccess = this.provider.success;
+      var monkeySuccess = function (response) {
+        if (providerSuccess !== undefined) {
+          response = providerSuccess.call(that, response) || response;
         }
-        if (this.provider.url === undefined && this.provider.success !== undefined) {
-            response = this.provider.success(response);
+        that.castAnchor.call(that, response, callback);
+      };
+
+      var providerError = this.provider.error;
+      var monkeyError = function (response) {
+        if (providerError !== undefined) {
+          response = providerError.call(that, response);
+          if (response === false) {
+            return false;
+          }
         }
+        that.castAnchor.call(that, response, callback);
+      };
 
-        if (Object.prototype.toString.call(response) === '[object Array]') {
-            response = {
-                array: response
-            };
-        }
+      that.ajaxParams = this.provider;
+      that.ajaxParams.success = monkeySuccess;
+      that.ajaxParams.error = monkeyError;
 
-        this.view = Barbe.templates[this.template].render(response, this.anchor);
-        return this.view;
-    };
+      Barbe.settings.ajax(that.ajaxParams);
+    }
+  };
 
-    /**
-     *  Render the template with the response and attach it to the anchor
-     *
-     *  @params response {object}       api response or data for the template
-     *  @params callback {castAnchor}   function called after
-     */
-    Barbe.View.prototype.castAnchor = function (response, callback) {
-        // TODO move this to the render function
-        this.loader && this.loader.remove();
-        var rendered = this.render(response);
-        callback && callback.call(this, rendered);
-        Barbe.settings.growCallback && Barbe.settings.growCallback(rendered);
-    };
+  /**
+   * Run Barbe to populate the template and attach it the anchor
+   *
+   * @param callback {function} function executed when the ajax call successed and the template has been populated
+   */
+  Barbe.View.prototype.grow = function (callback) {
+    if (
+      typeof this.provider === "function" ||
+      this.provider.url !== undefined
+    ) {
+      if (this.options.Loader) {
+        this.loader = new this.options.Loader(this.anchor);
+      }
+      this.ajax.call(this, callback);
+    } else {
+      this.castAnchor(this.provider.data, callback);
+    }
+  };
 
+  /**
+   * Initialize settings before grabbing templates
+   *
+   * @param settings  {object}    see Barbe.settings
+   */
+  Barbe.init = function (settings) {
+    for (var key in settings) {
+      value = settings[key];
+      if (Barbe.settings[key] !== undefined) {
+        Barbe.settings[key] = value;
+      }
+    }
 
-    /**
-     * Process the ajax call 
-     *
-     * @param callback {function} function executed when the ajax call successed and the template has been populated
-     */
-    Barbe.View.prototype.ajax = function (callback) {
-        var that = this;
-        if (typeof this.provider === "function") {
-            // Specific asynchronous function for this View
-            var monkeyProvider = function (response) {
-                that.castAnchor.call(that, response, callback);
-            };
-            this.provider.call(this, monkeyProvider);
-        } else {
-            // Use the global ajax function
-            var providerSuccess = this.provider.success;
-            var monkeySuccess = function (response) {
-                if (providerSuccess !== undefined) {
-                    response = providerSuccess.call(that, response) || response;
-                }
-                that.castAnchor.call(that, response, callback);
-            };
+    if (Barbe.settings.autoLoad) {
+      Barbe.grab();
+    }
+  };
 
-            var providerError = this.provider.error;
-            var monkeyError = function (response) {
-                if (providerError !== undefined) {
-                    response = providerError.call(that, response);
-                    if (response === false) {
-                        return false;
-                    }
-                }
-                that.castAnchor.call(that, response, callback);
-            };
-
-            that.ajaxParams = this.provider;
-            that.ajaxParams.success = monkeySuccess;
-            that.ajaxParams.error = monkeyError;
-
-            Barbe.settings.ajax(that.ajaxParams);
-        }
-    };
-
-    /**
-     * Run Barbe to populate the template and attach it the anchor
-     *
-     * @param callback {function} function executed when the ajax call successed and the template has been populated
-     */
-    Barbe.View.prototype.grow = function (callback) {
-        if (typeof this.provider === "function" || this.provider.url !== undefined) {
-            if (this.options.Loader) {
-                this.loader = new this.options.Loader(this.anchor);
-            }
-            this.ajax.call(this, callback);
-        } else {
-            this.castAnchor(this.provider.data, callback);
-        }
-    };
-
-
-    /**
-     * Initialize settings before grabbing templates
-     *
-     * @param settings  {object}    see Barbe.settings
-     */
-    Barbe.init = function (settings) {
-        for (var key in settings) {
-            value = settings[key];
-            if (Barbe.settings[key] !== undefined) {
-                Barbe.settings[key] = value;
-            }
-        }
-
-        if (Barbe.settings.autoLoad) {
-            Barbe.grab();
-        }
-    };
-
-    window.Barbe = Barbe;
-    
+  window.Barbe = Barbe;
 })(window);
--- js/cv.js
+++ js/cv.js
@@ -1,38 +1,111 @@
-function load_json() {
-    language = document.location.hash.replace('#', '') || 'english';
-
-    var file = language + '.json';
-    var success = function (data) {
-        data.category[0].experience.forEach(function (category) {
-            var date = {};
-            date.start = moment(category.date.start);
-            date.end = moment(category.date.end);
-            var startFormat = 'MMMM YYYY';
-            if (date.start.year() === date.end.year()) {
-                startFormat = 'MMMM';
-            }
-            category.date.start = date.start.format(startFormat);
-            category.date.end = category.date.end ? date.end.format('MMMM YYYY') : 'present';
-            category.date.duration = date.end.from(date.start, true);
-        });
-
-        var templateHeader = new Barbe.View('template-title', {data: data}).grow();
-
-        var templatePersonal = new Barbe.View("template-personal", {data: data}).grow();
-
-        var templateCategory = new Barbe.View("template-category", {data: data}).grow(function () {
-        });
-
-    };
-
-    $.ajax({
-        url: 'data/' + file,
-        dataType: 'json',
-        success: success,
-        error: function() { console.log(arguments); }
-    });
+function getMonth(d) {
+  return d.toLocaleString("en", {
+    month: "long",
+  });
 }
 
-window.addEventListener('hashchange', load_json);
+function duration(seconds) {
+  let output = [];
+  let remainder = parseInt(seconds, 10);
+  const durations = [
+    // Number of seconds in
+    365 * 24 * 60 * 60, // a year
+    30 * 24 * 60 * 60, // a month
+    24 * 60 * 60, // a day
+    60 * 60, // a hour
+    1 * 60, // a minute
+  ];
+  durations.forEach((divisor, index) => {
+    const quotient = Math.abs(parseInt(`${remainder / divisor}`, 10));
+    remainder = Math.abs(remainder % divisor);
+    output.push(quotient);
+    if (index === durations.length - 1) {
+      output.push(remainder);
+    }
+  });
+  return output;
+}
+
+const units = ["year", "month", "day", "hour", "minute", "second"];
+
+let qs = {
+  getAll: function () {
+    return [];
+  },
+};
+try {
+  qs = new URLSearchParams(location.search);
+} catch (exception) {
+  console.warn("failed to parse query parameters");
+}
+
+function load_json() {
+  language = document.location.hash.replace("#", "") || "english";
+
+  var file = language + ".json";
+  var success = function (data) {
+    data.category.forEach(function (category) {
+      if (category.experience) {
+        const highlights = qs.getAll(category.key);
+        category.experience = category.experience.filter(function (experience) {
+          if (highlights.length) {
+            return highlights.includes(experience.key);
+          } else {
+            return experience.highlight;
+          }
+        });
+        category.experience.forEach(function (experience) {
+          if (experience.date) {
+            var date = {};
+            date.start = new Date(experience.date.start);
+            date.end = experience.date.end
+              ? new Date(experience.date.end)
+              : new Date();
+            if (date.start.getFullYear() === date.end.getFullYear()) {
+              experience.date.start = getMonth(date.start);
+            } else {
+              experience.date.start = `${getMonth(
+                date.start
+              )} ${date.start.getFullYear()}`;
+            }
+            experience.date.end = experience.date.end
+              ? `${getMonth(date.end)} ${date.end.getFullYear()}`
+              : "present";
+            const d = duration(((date.end || new Date()) - date.start) / 1000);
+            const index = d.findIndex(function (v, index) {
+              return d[index] !== 0;
+            });
+            experience.date.duration = `${d[index]} ${
+              d[index] === 1 ? units[index] : `${units[index]}s`
+            }`;
+          }
+
+          if (experience.value) {
+            const highlights = qs.getAll(experience.key);
+            experience.value = experience.value.filter(function (v) {
+              if (highlights.length) {
+                return highlights.includes(v.key);
+              } else {
+                return v.highlight;
+              }
+            });
+          }
+        });
+      }
+    });
+
+    new Barbe.View("template-title", { data: data }).grow();
+    new Barbe.View("template-personal", { data: data }).grow();
+    new Barbe.View("template-category", { data: data }).grow(function () {});
+  };
+
+  fetch("data/" + file, {})
+    .then(function (response) {
+      return response.json();
+    })
+    .then(success);
+}
+
+window.addEventListener("hashchange", load_json);
 
 load_json();
